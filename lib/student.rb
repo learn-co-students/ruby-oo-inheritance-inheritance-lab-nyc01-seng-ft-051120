d797a14a06f148606d3aa03a39b2510dc72d2460
Gary Cordero <GaryCordero1690@gmail.com>
2020-05-17 17:56:19 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
+require_relative './user.rb'
+class Student < User
+    
+    attr_reader :knowledge
+    def initialize
+        super()
+        @knowledge = []
+    end
+    def learn(new_knowledge)
+        @knowledge << new_knowledge
+    end
+ 
 end
